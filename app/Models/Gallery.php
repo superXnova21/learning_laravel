3fc81ca0e241e5d31ffa15d9fa1174454a666196
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-20 19:51:05 +0600

    done uploading laravel

---
+++ app/Models/Gallery.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+
+class Gallery extends Model implements HasMedia
+{
+    use SoftDeletes, InteractsWithMedia;
+
+    public $table = 'galleries';
+
+    protected $appends = [
+        'photos',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')->width(50)->height(50);
+    }
+
+    public function getPhotosAttribute()
+    {
+        $files = $this->getMedia('photos');
+        $files->each(function ($item) {
+            $item->url       = $item->getUrl();
+            $item->thumbnail = $item->getUrl('thumb');
+        });
+
+        return $files;
+    }
+}
